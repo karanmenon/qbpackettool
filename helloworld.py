d9fe4f9afe7211f6f2aab6ca7342a57b06eca0d1
Karan Menon <karanmenon@karans-mbp-2.lan>
2021-05-02 21:44:13 -0400

    started coding

--- helloworld.py
+++ helloworld.py
@@ -1,1 +1,9 @@
+import os
+import sys
+import docx2txt
+folder_addr=""
+
+for path in pathlib.Path("a_directory").iterdir():
+    if path.is_file():
+        current_file=open(path, "r")
 print "Hello World"

faa4e7e00185c5aa78f97f576c3364e4c678bf6a
Karan Menon <karanmenon@karans-mbp-2.lan>
2021-05-02 22:39:31 -0400

    made arrays for categories

--- packetgen.py
+++ packetgen.py
@@ -2,10 +2,11 @@ import os
 import sys
 import docx2txt 
 '''this library is necessary, you must pip install docx2txt 
-in your command line so that we can read word docuemtns
+in your command line so that we can read word documents
 '''
-folder_addr=""
+folder_addr="" 
 files=[]
+
 sci=[]
 hist=[]
 lit=[]
@@ -15,8 +16,23 @@ geo=[]
 ce=[]
 misc=[]
 trash=[]
+
+sci_bonus=[]
+hist_bonus=[]
+lit_bonus=[]
+fa_bonus=[]
+rmpss_bonus=[]
+geo_bonus=[]
+ce_bonus=[]
+misc_bonus=[]
+trash_bonus=[]
+
 for file in os.listdir(folder_addr):
     if file.endswith('.docx'):
         files.append(file)
 for i in range(len(files)):
+    all_tossups=[]
+    all_bonuses=[]
     text=docx2txt.process(files[i])
+
+
